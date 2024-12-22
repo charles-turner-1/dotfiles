d241ded24d3d4e5a748e2ec563ae3bfd2ca6cdb9
Charles Turner <charles.turner@anu.edu.au>
2024-12-22 13:40:52 +0800

    Cleaning out config

--- nvim/lua/ct/core/keymaps.lua
+++ nvim/lua/ct/core/keymaps.lua
@@ -26,3 +26,23 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current t
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- telescope
+--
+vim.keymap.set("n", "<leader>ff", ":Telescope find_files<cr>", { desc = "Fuzzy find files" })
+vim.keymap.set("n", "<leader>fp", ":Telescope git_files<cr>", { desc = "Fuzzy find git files" })
+vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>", { desc = "Live grep" })
+vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>", { desc = "Find old files" })
+vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<cr>", { desc = "Fuzzy find help" })
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<cr>", { desc = "Fuzzy buffer search" })
+
+-- tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeFindFileToggle<cr>", { desc = "Toggle explorer" })
+
+-- format code using LSP
+vim.keymap.set("n", "<leader>fmd", vim.lsp.buf.format)
+
+-- markdown preview
+vim.keymap.set("n", "<leader>mdp", ":MarkdownPreviewToggle<cr>")
+
+-- nvim-comment
+vim.keymap.set({ "n", "v" }, "<C-/>", ":CommentToggle<cr>", { desc = "Comment/ uncomment line" })
